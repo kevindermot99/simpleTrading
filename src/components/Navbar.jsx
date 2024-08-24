b9f7da200370f4a4dc1ce5cdd8874b35fc990b28
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-24 22:33:56 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,8 @@ function Navbar() {
   return (
     <div className='min-h-[66px] px-5 h-fit w-full flex items-center justify-between'>
         {/* Logo & Links */}
-        <div className='flex items-center h-full w-fit'>
-            <h1 className='text-[24px] font-extrabold tracking-normal text-main-color'>Simple Trading</h1>
+        <div className='flex items-center h-fit w-fit'>
+            <h1 className='text-2xl font-bold tracking-tight text-main-color'>Simple Trading</h1>
         </div>
         {/* Search, Notifications and Profile image */}
         <div></div>
