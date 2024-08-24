e0f51e8d5c5847ee5f43b8a7ff5f6857c55dcccd
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-24 21:49:05 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div className='min-h-[66px] h-fit w-full flex items-center'>Navbar</div>
+    <div className='min-h-[66px] h-fit w-full flex items-center justify-between'>
+        {/* Logo & Links */}
+        <div></div>
+        {/* Search, Notifications and Profile image */}
+        <div></div>
+    </div>
   )
 }
 
