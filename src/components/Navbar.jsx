309098e3c430a988fe3343a793fc0fd3dc10f675
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-25 14:39:08 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,29 +12,29 @@ function Navbar() {
   }, []);
 
   return (
-    <div className={`h-[65px] px-16 border-b-[1px]  w-full flex items-center justify-between tracking-tight transition-all ${scrolled ? 'text-text-color-black border-border-lines-light sticky top-0' : 'text-text-color-white border-transparent sticky top-[-100%]'} `}>
+    <div className={`h-[65px] px-16 w-full flex items-center justify-between tracking-tight sticky top-0 transition-all ${scrolled ? 'text-text-color-black bg-body-white' : 'text-text-color-white main-header'} `}>
       {/* Logo & Links */}
       <div className='flex items-center h-fit w-fit gap-1'>
         <h1 className='font-bold text-3xl font-DMSerifText mr-4'>st.</h1>
         <div className='flex items-center'>
-          <Link to="/" className='text-sm px-3 py-2 hover:bg-border-lines-light/70'>Dashboard</Link>
-          <Link to="/" className='text-sm px-3 py-2 hover:bg-border-lines-light/70'>Leaders</Link>
-          <Link to="/" className='text-sm px-3 py-2 hover:bg-border-lines-light/70'>Find Agents</Link>
-          <Link to="/" className='text-sm px-3 py-2 hover:bg-border-lines-light/70'>Signals</Link>
-          <Link to="/" className='text-sm px-3 py-2 hover:bg-border-lines-light/70'>Learn</Link>
-          <Link to="/" className='text-sm px-3 py-2 hover:bg-border-lines-light/70'>Help Center</Link>
+          <Link to="/" className={`text-sm px-3 py-2 ${scrolled ? 'hover:bg-stone-300/40' : 'hover:bg-stone-300/10' }`}>Dashboard</Link>
+          <Link to="/" className={`text-sm px-3 py-2 ${scrolled ? 'hover:bg-stone-300/40' : 'hover:bg-stone-300/10' }`}>Leaders</Link>
+          <Link to="/" className={`text-sm px-3 py-2 ${scrolled ? 'hover:bg-stone-300/40' : 'hover:bg-stone-300/10' }`}>Find Agents</Link>
+          <Link to="/" className={`text-sm px-3 py-2 ${scrolled ? 'hover:bg-stone-300/40' : 'hover:bg-stone-300/10' }`}>Signals</Link>
+          <Link to="/" className={`text-sm px-3 py-2 ${scrolled ? 'hover:bg-stone-300/40' : 'hover:bg-stone-300/10' }`}>Learn</Link>
+          <Link to="/" className={`text-sm px-3 py-2 ${scrolled ? 'hover:bg-stone-300/40' : 'hover:bg-stone-300/10' }`}>Help Center</Link>
         </div>
       </div>
       {/* Search, Notifications and Profile image */}
       <div className='flex items-center'>
-        <Link to="/" className='text-xl p-2 hover:bg-border-lines-light/70'>
+        <Link to="/" className={`text-xl p-2 hover:bg-border-lines-light/70 ${scrolled ? 'hover:bg-stone-300/40' : 'hover:bg-stone-300/10' }`}>
           <LuSearch />
         </Link>
-        <Link to="/" className='text-xl p-2 hover:bg-border-lines-light/70'>
+        <Link to="/" className={`text-xl p-2 hover:bg-border-lines-light/70 ${scrolled ? 'hover:bg-stone-300/40' : 'hover:bg-stone-300/10' }`}>
           <LuBell />
         </Link>
         <Link to="/" className='text-sm flex items-center gap-0 ml-2'>
-          <div className='h-[30px] text-xs w-auto aspect-square flex items-center justify-center font-bold rounded-full bg-border-lines-light'>
+          <div className='h-[30px] text-xs w-auto aspect-square flex items-center justify-center font-bold rounded-full bg-border-lines-light text-text-color-black'>
             KD
           </div>
           <span className='ml-2 max-w-[80px] truncate'>Kevin Mbonimpaye Kaneza</span>
