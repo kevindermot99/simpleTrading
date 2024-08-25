fa8aff2c409618c154c63a5100e5da8e6493e275
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-25 13:52:52 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
-import Logo from '../assets/logo.png'
+import { Link } from 'react-router-dom'
 
 function Navbar() {
   return (
-    <div className='min-h-[66px] px-10   h-fit w-full flex items-center justify-between text-text-color-black'>
+    <div className='h-[65px] px-16 border-b-[1px] border-border-lines-light w-full flex items-center justify-between text-text-color-black tracking-tight'>
       {/* Logo & Links */}
-      <div className='flex items-end h-fit w-fit gap-1'>
-        <h1 className='font-bold text-3xl font-DMSerifText'>st.</h1>
+      <div className='flex items-center h-fit w-fit gap-1'>
+        <h1 className='font-bold text-3xl font-DMSerifText mr-4'>st.</h1>
         <div className='flex items-center'>
-
+            <Link to="/" className='text-sm px-3 py-2 hover:bg-border-lines-light'>Dashboard</Link>
+            <Link to="/" className='text-sm px-3 py-2 hover:bg-border-lines-light'>Dashboard</Link>
+            <Link to="/" className='text-sm px-3 py-2 hover:bg-border-lines-light'>Dashboard</Link>
         </div>
       </div>
       {/* Search, Notifications and Profile image */}
