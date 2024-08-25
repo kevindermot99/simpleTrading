8a40a478e54ba48922464a4a2630977e4280394d
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-25 20:06:38 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { HashRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import Transactions from './pages/Transactions'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
       <HashRouter>
         <Routes>
           <Route path={`/`} element={<Home />} />
+          <Route path={`/transactions`} element={<Transactions />} />
         </Routes>
       </HashRouter>
     </>
