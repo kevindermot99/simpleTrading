8175c617ed4c18d85a57f27b87ea7d27df96c4b5
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-25 14:48:47 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,10 @@ function Home() {
     <div className='bg-body-white w-full h-full min-h-[3000px] relative'>
         <Navbar />
         {/* Hero */}
-        <div className='main-header min-h-[300px] z-50'>
+        <div className='main-header min-h-[200px] px-10 pt-5 text-text-color-white tracking-tight'>
+            {/* Greetings */}
+            <h1 className=' font-medium text-xl'>Welcome back, Petter!</h1>
+            <div className='flex'></div>
         </div>
     </div>
   )
