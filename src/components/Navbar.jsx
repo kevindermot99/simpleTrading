17fec166dce44e900a1e72dac607d4384a916701
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-25 13:40:20 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
+import Logo from '../assets/logo.png'
 
 function Navbar() {
   return (
-    <div className='min-h-[66px] px-5 h-fit w-full flex items-center justify-between'>
-        {/* Logo & Links */}
-        <div className='flex items-center h-fit w-fit'>
-            <h1 className='text-2xl font-bold tracking-tight text-main-color'>Simple Trading</h1>
+    <div className='min-h-[66px] px-10   h-fit w-full flex items-center justify-between text-text-color-black'>
+      {/* Logo & Links */}
+      <div className='flex items-end h-fit w-fit gap-1'>
+        <h1 className='font-bold text-3xl font-DMSerifText'>st.</h1>
+        <div className='flex items-center'>
+
         </div>
-        {/* Search, Notifications and Profile image */}
-        <div></div>
+      </div>
+      {/* Search, Notifications and Profile image */}
+      <div></div>
     </div>
   )
 }
