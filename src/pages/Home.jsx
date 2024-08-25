a6a13b5cd7ef2c7c2d00a5f4d515da24dae38f25
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-25 14:39:10 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,11 @@ import Navbar from '../components/Navbar'
 
 function Home() {
   return (
-    <div className='bg-body-white w-full h-full min-h-[3000px]'>
+    <div className='bg-body-white w-full h-full min-h-[3000px] relative'>
         <Navbar />
+        {/* Hero */}
+        <div className='main-header min-h-[300px] z-50'>
+        </div>
     </div>
   )
 }
