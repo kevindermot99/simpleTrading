90f905eb725e5f2bd06ded01ee5ece4506c3c4d8
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-25 14:20:29 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { LuChevronDown, LuUser2 } from 'react-icons/lu'
 import { Link } from 'react-router-dom'
 
 function Navbar() {
@@ -8,16 +9,25 @@ function Navbar() {
       <div className='flex items-center h-fit w-fit gap-1'>
         <h1 className='font-bold text-3xl font-DMSerifText mr-4'>st.</h1>
         <div className='flex items-center'>
-            <Link to="/" className='text-sm px-3 py-2 hover:bg-border-lines-light'>Dashboard</Link>
-            <Link to="/" className='text-sm px-3 py-2 hover:bg-border-lines-light'>Leaders</Link>
-            <Link to="/" className='text-sm px-3 py-2 hover:bg-border-lines-light'>Find Agents</Link>
-            <Link to="/" className='text-sm px-3 py-2 hover:bg-border-lines-light'>Signals</Link>
-            <Link to="/" className='text-sm px-3 py-2 hover:bg-border-lines-light'>Learn</Link>
-            <Link to="/" className='text-sm px-3 py-2 hover:bg-border-lines-light'>Help Center</Link>
+          <Link to="/" className='text-sm px-3 py-2 hover:bg-border-lines-light/70'>Dashboard</Link>
+          <Link to="/" className='text-sm px-3 py-2 hover:bg-border-lines-light/70'>Leaders</Link>
+          <Link to="/" className='text-sm px-3 py-2 hover:bg-border-lines-light/70'>Find Agents</Link>
+          <Link to="/" className='text-sm px-3 py-2 hover:bg-border-lines-light/70'>Signals</Link>
+          <Link to="/" className='text-sm px-3 py-2 hover:bg-border-lines-light/70'>Learn</Link>
+          <Link to="/" className='text-sm px-3 py-2 hover:bg-border-lines-light/70'>Help Center</Link>
         </div>
       </div>
       {/* Search, Notifications and Profile image */}
-      <div></div>
+      <div className='flex items-center'>
+        <Link to="/" className='text-sm px-3 py-2 hover:bg-border-lines-light/70'></Link>
+        <Link to="/" className='text-sm flex items-center gap-0'>
+          <div className='h-[30px] text-xs w-auto aspect-square flex items-center justify-center font-bold rounded-full bg-border-lines-light'>
+            KD
+          </div>
+          <span className='ml-2 max-w-[80px] truncate'>Kevin Mbonimpaye Kaneza</span>
+          <LuChevronDown className='text-base' />
+        </Link>
+      </div>
     </div>
   )
 }
