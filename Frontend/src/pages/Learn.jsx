31b18bf1b2e771c60e2d935883afde56ddd6fcd7
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-31 21:35:50 +0200

    Create Learn.jsx

---
+++ Frontend/src/pages/Learn.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import { Link } from "react-router-dom";
+import { Input } from 'antd';
+import { DatePicker, Space } from 'antd';
+const { RangePicker } = DatePicker;
+
+function Learn() {
+    return (
+        <div className="bg-body-white w-full h-full min-h-svh relative">
+            <Navbar dynamic="on" />
+            {/* Hero */}
+            <div className="main-header min-h-[170px] px-10 pt-5 text-text-color-white tracking-tight">
+                {/* Greetings */}
+                <h1 className=" font-medium text-xl">Join Forex Pools</h1>
+                <div className="flex items-center gap-1 my-3">
+                    <button
+                        className={`text-sm px-4 py-2 font-medium hover:bg-stone-300/10 border-b-2 border-text-color-white`}
+                    >
+                        All 
+                    </button>
+                </div>
+            </div>
+            <div className="flex gap-5 w-full h-full bg-transparent mt-[-50px] px-10 text-text-color-black pb-10">
+                <div className="w-full min-h-full flex flex-col gap-5">
+                    <div className=" w-full flex-1 h-full min-h-[500px] bg-white shadow-lg shadow-stone-600/10 flex flex-col gap-2 p-6">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+export default Learn;
