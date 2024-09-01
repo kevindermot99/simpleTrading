e20204be37cd39edf0881c438a606ec0971b8d8d
kevindermot99 <mbonimpayekevin@gmail.com>
2024-09-01 15:11:06 +0200

    Update LearnVideos.jsx

--- Frontend/src/pages/LearnVideos.jsx
+++ Frontend/src/pages/LearnVideos.jsx
@@ -157,7 +157,7 @@ function LearnVideos() {
                                     </div>
                                     <div className="flex flex-col items-start justify-between gap-1 h-full w-full">
                                         <div className="flex flex-col items-start justify-start gap-1">
-                                            <h1 className="break-words font-semibold tracking-tight leading-5 whitespace-normal break-all">
+                                            <h1 className="break-words font-semibold tracking-tight leading-5 whitespace-normal max-md:">
                                                 {book.name}
                                             </h1>
                                             <p className="text-xs text-text-color-black/70">Book By {book.by}</p>
