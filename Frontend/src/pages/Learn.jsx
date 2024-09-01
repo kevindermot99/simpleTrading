ccc519325bdf87d6d2f692c6c7cc9ead0e8dc5d1
kevindermot99 <mbonimpayekevin@gmail.com>
2024-09-01 13:35:32 +0200

    Update Learn.jsx

--- Frontend/src/pages/Learn.jsx
+++ Frontend/src/pages/Learn.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import { Link } from "react-router-dom";
-import { Input } from 'antd';
+import { ConfigProvider, Input, Select } from 'antd';
 import { DatePicker, Space } from 'antd';
 const { RangePicker } = DatePicker;
 
@@ -34,7 +34,43 @@ function Learn() {
             <div className="flex gap-5 w-full h-full bg-transparent mt-[-50px] px-10 text-text-color-black pb-10">
                 <div className="w-full min-h-full flex flex-col gap-5">
                     <div className=" w-full flex-1 h-full min-h-[500px] bg-white shadow-lg shadow-stone-600/10 flex flex-col gap-2 p-6">
-                        
+                        <div className="flex itec justify-between w-full">
+                            <h1 className="text-sm text-text-color-black/60 font-medium">
+                                Available Books
+                            </h1>
+                            <form className="flex items-center gap-2">
+                                <ConfigProvider
+                                    theme={{
+                                        token: {
+                                            // Seed Token
+                                            colorPrimary: "#21232a",
+                                            colorPrimaryBg: "#e8e8e8",
+                                            borderRadius: 2,
+                                            colorBorder: "#00000010",
+                                            colorBgContainer: "#e5e7eb70",
+                                            zIndexPopupBase: "1",
+                                            zIndexBase: "1"
+                                        },
+                                    }}
+                                >
+                                    <Select
+                                        defaultValue="All ranges"
+                                        style={{
+                                            width: "150px",
+                                            boxShadow: "none",
+                                        }}
+                                        allowClear
+                                        options={[
+                                            { value: "Recommended", label: "Recommended", },
+                                            { value: "Best Sellers", label: "Best Sellers" },
+                                            { value: "Essential Reads", label: "Essential Reads" }
+                                        ]}
+                                        placeholder="Currency"
+                                    />
+                                    <Input placeholder="Search Book name" style={{ width: "230px" }} />
+                                </ConfigProvider>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
